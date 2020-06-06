aab9f79bae7c8c5f4430815b0e68aa8e7dfb88e4
dhk1349 <dhk1349@naver.com>
2020-06-07 02:49:05 +0900

    .

---
+++ lolapi/dbtest.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  7 02:35:59 2020
+
+@author: dhk13
+"""
+
+import cx_Oracle
+
+#한글 지원 방법
+import os
+os.putenv('NLS_LANG', '.UTF8')
+
+#연결에 필요한 기본 정보 (유저, 비밀번호, 데이터베이스 서버 주소)
+connection = cx_Oracle.connect('admin','','lolgg.culykwl5fmgv.ap-northeast-2.rds.amazonaws.com:1521/ORCL')
+
+cursor = connection.cursor()
+
+cursor.execute("""
+   select *
+   from tab
+   """
+)
+
+for name in cursor:
+   print("테스트 이름 리스트 : ", name)
--- lolapi/funtions.py
+++ lolapi/funtions.py
@@ -10,3 +10,7 @@ Created on Sat Jun  6 01:53:30 2020
 API로 가져온 데이터를 revise하는 함수가 필요.
 -경기 결과를 집계하는 함수
 """
+
+def GameDataParser(path):
+    #Output으로 받은 txt파일을 파싱하는 함수 
+    return
